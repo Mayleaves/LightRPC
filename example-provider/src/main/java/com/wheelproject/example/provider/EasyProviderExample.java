130c8c0313673b77e863260c0f2a6b1d51665cb8
xxy <xingyue.xia@outlook.com>
2025-05-02 18:14:39 +0800

    day1：基本功能完成

---
+++ example-provider/src/main/java/com/wheelproject/example/provider/EasyProviderExample.java
@@ -0,0 +1,19 @@
+package com.wheelproject.example.provider;
+
+import com.wheelproject.example.common.service.UserService;
+import com.wheelproject.rpc.registry.LocalRegistry;
+import com.wheelproject.rpc.server.HttpServer;
+import com.wheelproject.rpc.server.VertxHttpServer;
+
+/**
+ * 简易服务提供者示例
+ */
+public class EasyProviderExample {
+    public static void main(String[] args){
+        // 注册服务
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
+        // 启动 web 服务
+        HttpServer httpServer = new VertxHttpServer();
+        httpServer.doStart(8080) ;
+    }
+}
---
+++ example-provider/src/main/java/com/wheelproject/example/provider/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.wheelproject.example.provider;
+
+import com.wheelproject.example.common.model.User;
+import com.wheelproject.example.common.service.UserService;
+
+/**
+ * 用户服务实现类
+ */
+public class UserServiceImpl implements UserService{
+    public User getUser(User user){
+        System.out.println("用户名：" + user.getName());
+        return user;
+    }
+}
