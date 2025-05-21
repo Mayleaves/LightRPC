14b9f1d155f2e9945ecf21c30112d10d0139ac19
xxy <xingyue.xia@outlook.com>
2025-05-21 14:13:25 +0800

    ectd注册中心：更正 getServiceNodeKey

--- rpc-core/src/main/java/com/wheelproject/rpc/model/ServiceMetaInfo.java
+++ rpc-core/src/main/java/com/wheelproject/rpc/model/ServiceMetaInfo.java
@@ -55,7 +55,7 @@ public class ServiceMetaInfo {
      * @return
      */
     public String getServiceNodeKey() {
-        return String.format("%s/%s:", getServiceKey(), serviceAddress);
+        return String.format("%s/%s:%s", getServiceKey(), serviceHost, servicePort);
     }
 
 
